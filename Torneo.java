b1f648479dc9dd5eb0846c7e48fa00650f473e70
Cristhian Eduardo Osorio Restrepo <osoriocristhian86@gmail.com>
2023-10-22 10:29:06 -0500

    otras coasas del UML (faltan las fechas no se)

--- Torneo.java
+++ Torneo.java
@@ -1,3 +1,15 @@
 public class Torneo {
+    private String nombreTorneo;
+    //arraylist equipos
+    //fecha inicio (Date)
+    private int limiteEdad;
+    private String tipoDeporte;
+    private int numeroJugadoresEquipo;
+    private Genero genero;
+    private TipoTorneo tipoTorneo;
+    //fecha inscripcion (Date)
+    //fecha cierre inscripcion (Date)
+    private float inscripcion;
+    
     
 }
