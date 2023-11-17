04b0ad2a277dd3e3629817bbc2d99c3114de8ac9
Camilo Ospina <camiloa.ospinaa@uqvirtual.edu.co>
2023-11-17 08:49:13 -0500

    Falta

--- Principal.java
+++ Principal.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 import javax.swing.JOptionPane;
 
 /**
- * AUTORES: 
+ * AUTORES:
  * Daniel Perez
  * Camilo Ospina (El mas guapo)
  * Cristhian Osorio
@@ -15,33 +15,33 @@ public class Principal {
         // el menu va a ser de la siguiente manera:
 
         /**
-         * MENU 
+         * MENU
          * 1. Crear torneo
          * 2. Entrar a torneo
          * 3. salir
          * 
-         * 1. en crear torneo se va a llamar al constructor por asi decirlo 
+         * 1. en crear torneo se va a llamar al constructor por asi decirlo
          * 
          * 2.
-         *  a. Añadir equipo, cuando se unda añadir equipo se llama comparar fecha
-         *  b. modificar fechas
-         *    
-         *  c. enfrentamientos 
-         *  d. equipos 
-         *  e. jueces 
-         *  f. volver 
+         * a. Añadir equipo, cuando se unda añadir equipo se llama comparar fecha
+         * b. modificar fechas
+         * 
+         * c. enfrentamientos
+         * d. equipos
+         * e. jueces
+         * f. volver
          * 
          * C. en c se llenarian los campos referente a los equipos y todo lo demas
          * 
-         * 1. general 
-         * 2. buscar por equipos  
-         * 3. buscar por juez 
-         * 4. resultados  
-         * 5. modificar enfrentamientos 
+         * 1. general
+         * 2. buscar por equipos
+         * 3. buscar por juez
+         * 4. resultados
+         * 5. modificar enfrentamientos
          * 
          */
 
-        int op = 0; 
+        int op = 0;
         do {
             op = Integer.parseInt(pedir("1. Crear torneo\n 2. Entrar al torneo\n 3. salir", null));
 
@@ -52,12 +52,22 @@ public class Principal {
                 String deporte = pedir("ingrese el deporte del torneo", null);
                 int limiteJugador = pedirInt("Ingrese el numero de jugadores maximo", null);
                 String gen = pedir("ingrese el genero del torneo: \nMASCULINO \nFEMENINO \nMIXTO;", null);
-                if (gen.equalsIgnoreCase("MASCULINO")) {
-                    
-                }
+                Genero genero = null;
+                do {
 
+                    if (gen.equalsIgnoreCase("MASCULINO")) {
+                        genero = Genero.MASCULINO;
+                    }
+                    if (gen.equalsIgnoreCase("FEMENINO")) {
+                        genero = Genero.FEMENINO;
+                    }
+                    if (gen.equalsIgnoreCase("MIXTO")) {
+                        genero = Genero.MIXTO;
+                    }
+                } while (genero == null);
 
-                Torneo torneo = new Torneo(nombre, fecha, limiteEdad, deporte, limiteJugador, null, null, null, null, op);
+                Torneo torneo = new Torneo(nombre, fecha, limiteEdad, deporte, limiteJugador, genero, null, null, null,
+                        op);
 
             }
             if (op == 2) {
@@ -65,8 +75,9 @@ public class Principal {
                 int option = 0;
                 do {
                     option = Integer.parseInt(pedir(
-                            "1. Añadir equipo\n 2. modificar fechas\n 3. enfrentamientos\n 4. equipos\n 5. jueces\n 6. volver", null));
-                    
+                            "1. Añadir equipo\n 2. modificar fechas\n 3. enfrentamientos\n 4. equipos\n 5. jueces\n 6. volver",
+                            null));
+
                     if (option == 1) {
 
                     }
@@ -78,27 +89,28 @@ public class Principal {
 
                         do {
                             optionn = Integer.parseInt(pedir(
-                            "1. general\n 2. buscar por equipos\n 3. buscar por juez\n 4. resultados\n 5. modificar enfrentamientos\n 6. volver", null));
+                                    "1. general\n 2. buscar por equipos\n 3. buscar por juez\n 4. resultados\n 5. modificar enfrentamientos\n 6. volver",
+                                    null));
                         } while (optionn != 6);
 
                         if (option == 4) {
 
-                         }
-                         if (option == 5) {
-
-                         }
-                         if (option == 6) {
-
-                         }
-                         if (option == 4) {
+                        }
+                        if (option == 5) {
 
                         }
-                         if (option == 5) {
+                        if (option == 6) {
 
                         }
-                         if (option == 6) {
-                        
-                         }
+                        if (option == 4) {
+
+                        }
+                        if (option == 5) {
+
+                        }
+                        if (option == 6) {
+
+                        }
                     }
                     if (option == 4) {
 
@@ -107,20 +119,18 @@ public class Principal {
 
                     }
                     if (option == 6) {
-                        
+
                     }
                 } while (option != 6);
 
             }
             if (op == 3) {
-                System.exit(0);    
-            }    
-         } while (op != 3);
-
-
+                System.exit(0);
+            }
+        } while (op != 3);
 
     }
-  
+
     public static String pedir(String msj, String text) {
         return JOptionPane.showInputDialog(null, msj, text);
     }
@@ -146,6 +156,7 @@ public class Principal {
             return pedirFloat("Error al ingresar\n" + msj, numero);
         }
     }
+
     public static LocalDateTime pedirFechaHora(String msj, String text) {
         String fecha = JOptionPane.showInputDialog(null,
                 msj + "en este formato con barras, puntos y comas (YYYY/MM/DD, HH:mm)", text);
@@ -160,4 +171,3 @@ public class Principal {
     }
 
 }
-
